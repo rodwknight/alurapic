58dbf9eb47b2fb930e25a12706d026b620a65248
Rodrigo Widmann <r.widmann@totvs.com.br>
2025-04-09 14:22:56 -0300

    ajustando prjeto para angular mais novo

--- src/app/photos/photo/photo.module.ts
+++ src/app/photos/photo/photo.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { PhotoComponent } from './photo.component';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient  } from '@angular/common/http';
 
 @NgModule({
     declarations: [PhotoComponent],
     imports: [
-        CommonModule,
-        HttpClientModule
+        CommonModule,        
     ],
+    providers: [provideHttpClient()],
     exports: [ PhotoComponent ]
 })
 export class PhotoModule { }
